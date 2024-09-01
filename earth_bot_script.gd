70a4c552da9d31960bbaa346cd1c5b463bde5924
BearWithMe <happyhammers513@gmail.com>
2024-09-01 19:10:44 +0200

    main menu fix, added scripts for opponnent

--- earth_bot_script.gd
+++ earth_bot_script.gd
@@ -27,28 +27,44 @@ func _on_start_of_match_timeout() -> void:
 	
 	
 @onready var probing_shot_timer = $probing_shot
+
+
 func _on_probing_shot_timeout() -> void:
 	while abs(Vector2(earth.position.x-mars.position.x, earth.position.y-mars.position.y).length()) < 400:
 		#print(Vector2(earth.position.x-mars.position.x, earth.position.y-mars.position.y).length())
 		#print("IM WAITING")
 		await get_tree().create_timer(0.5).timeout
 	
+	
+	if mars.find_child("Population") != null:
+		mars.find_child("Population").dmgTag = false
+	
 	earth_launcher.launch_missile(mars)
 	print("first missile fired")
 	
-	#IF earth takes dmg => launch 2 more
 	await get_tree().create_timer(2.0).timeout # replace with if
 
-	while abs(Vector2(earth.position.x-mars.position.x, earth.position.y-mars.position.y).length()) < 400:
-		#print(Vector2(earth.position.x-mars.position.x, earth.position.y-mars.position.y).length())
-		#print("IM WAITING")
-		await get_tree().create_timer(0.5).timeout
 	
-	earth_launcher.launch_missile(mars)
-	await get_tree().create_timer(0.5).timeout
-	earth_launcher.launch_missile(mars)
+	if mars.find_child("Population") != null && mars.find_child("Population").dmgTag:
+		print("doubletap")
+		
+		
+		
+		
+		#IF earth takes dmg => launch 2 more
 
-	probing_shot_timer.wait_time = randi_range(20,30)
+		while abs(Vector2(earth.position.x-mars.position.x, earth.position.y-mars.position.y).length()) < 400:
+			#print(Vector2(earth.position.x-mars.position.x, earth.position.y-mars.position.y).length())
+			#print("IM WAITING")
+			await get_tree().create_timer(0.5).timeout
+		
+		earth_launcher.launch_missile(mars)
+		await get_tree().create_timer(0.5).timeout
+		earth_launcher.launch_missile(mars)
+		
+		mars.find_child("Population").dmgTag = false
+	
+	probing_shot_timer.wait_time = randi_range(30,45)
 	
 
 @onready var on_melee_timer = $melee_asteroid
@@ -65,6 +81,13 @@ func _on_melee_asteroid_timeout() -> void:
 @onready var random_lazer_timer = $random_lazer_poke
 func _on_random_lazer_poke_timeout() -> void:
 	earth_lazer.initialize()
-	on_melee_timer.wait_time = randi_range(10,30)
+	on_melee_timer.wait_time = randi_range(30,40)
 
 	pass # Replace with function body.
+
+
+func _on_rebuild_shields_timeout() -> void:
+	earth_shield.addShield()
+	await get_tree().create_timer(2.0).timeout
+	earth_shield.addShield()
+	
